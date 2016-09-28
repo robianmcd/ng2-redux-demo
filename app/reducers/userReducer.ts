16a6e493a1e883f226af706a4bb0be85c50100c8
Rob McDiarmid <rmcdiarmid@konradgroup.com>
2016-09-28 11:01:34 -0400

    moved faker into user model

--- app/reducers/userReducer.ts
+++ app/reducers/userReducer.ts
@@ -1,20 +1,10 @@
 import { ActionReducer, Action } from '@ngrx/store';
 import {User} from "../models/user";
 
-declare let faker;
-
 export const userReducer: ActionReducer<User[]> = (users: User[] = [], action: Action) => {
     switch (action.type) {
         case 'CREATE_USER':
-            let newUser = new User({
-                id: faker.random.uuid(),
-                firstName: faker.name.firstName(),
-                lastName: faker.name.lastName(),
-                address: faker.address.streetAddress(),
-                email: faker.internet.email(),
-                avatar: faker.image.avatar()
-            });
-            return [...users, newUser];
+            return [...users, User.fromMockData()];
 
         case 'DELETE_USER':
             return users.filter(user => user.id !== action.payload.id);
