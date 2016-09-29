6dc7f02e4c34303f77faf306d8d14aaf714cd3d8
Rob McDiarmid <rmcdiarmid@konradgroup.com>
2016-09-29 15:33:22 -0400

    fixed indentation

--- app/userSearchResults.component.ts
+++ app/userSearchResults.component.ts
@@ -5,25 +5,28 @@ import {User} from "./models/user";
     selector: 'user-search-results',
     template: `
 <div class="panel panel-info" *ngFor="let user of users">
-  <div class="panel-heading"><strong>{{user.firstName}} {{user.lastName}}</strong></div>
-  <div class="panel-body">
-    <div class="col-sm-3">
-        <img [src]="user.avatar" style="display: block; margin-left: auto; margin-right: auto;">
+    <div class="panel-heading"><strong>{{user.firstName}} {{user.lastName}}</strong></div>
+    <div class="panel-body">
+        <div class="col-sm-3">
+            <img [src]="user.avatar" style="display: block; margin-left: auto; margin-right: auto;">
+        </div>
+        <div class="col-sm-9">
+            <table class="table">
+                <tr>
+                    <td><strong>ID</strong></td>
+                    <td>{{user.id}}</td>
+                </tr>
+                <tr>
+                    <td><strong>Address</strong></td>
+                    <td>{{user.address}}</td>
+                </tr>
+                <tr>
+                    <td><strong>Email</strong></td>
+                    <td>{{user.email}}</td>
+                </tr>
+            </table>
+        </div>
     </div>
-    <div class="col-sm-9">
-        <table class="table">
-            <tr>
-                <td><strong>ID</strong></td> <td>{{user.id}}</td>
-            </tr>
-            <tr>
-                <td><strong>Address</strong></td> <td>{{user.address}}</td>
-            </tr>
-            <tr>
-                <td><strong>Email</strong></td> <td>{{user.email}}</td>
-            </tr>
-        </table>
-    </div>
-  </div>
 </div>
 `
 })
